012261db8a95684d121f4b9b7348a18115f964be
Ansgar Nell <fixed-term.Ansgar.Nell@de.bosch.com>
2019-09-26 11:30:32 +0200

    Added entities for Product, Review and Comment.

---
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/entity/Comment.java
@@ -0,0 +1,67 @@
+package com.udacity.course3.reviews.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String commentText;
+    private Timestamp createdTs;
+    @ManyToOne
+    @JoinColumn
+    private Review review;
+
+    public Comment() {
+    }
+
+    public Comment(String title, String commentText, Timestamp createdTs, Review reviewId) {
+        this.title = title;
+        this.commentText = commentText;
+        this.createdTs = createdTs;
+        this.review = reviewId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public Timestamp getCreatedTs() {
+        return createdTs;
+    }
+
+    public void setCreatedTs(Timestamp createdTs) {
+        this.createdTs = createdTs;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
+    }
+}
---
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/entity/Review.java
@@ -0,0 +1,77 @@
+package com.udacity.course3.reviews.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String review_text;
+    private Timestamp createdTs;
+    private boolean recommended;
+    @ManyToOne
+    @JoinColumn
+    private Product product;
+
+    public Review() {
+    }
+
+    public Review(String title, String review_text, Timestamp createdTs, boolean recommended, Product productId) {
+        this.title = title;
+        this.review_text = review_text;
+        this.createdTs = createdTs;
+        this.recommended = recommended;
+        this.product = productId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReview_text() {
+        return review_text;
+    }
+
+    public void setReview_text(String review_text) {
+        this.review_text = review_text;
+    }
+
+    public Timestamp getCreatedTs() {
+        return createdTs;
+    }
+
+    public void setCreatedTs(Timestamp createdTs) {
+        this.createdTs = createdTs;
+    }
+
+    public boolean isRecommended() {
+        return recommended;
+    }
+
+    public void setRecommended(boolean recommended) {
+        this.recommended = recommended;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
